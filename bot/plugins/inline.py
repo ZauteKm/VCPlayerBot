6b877cc9b520dce611c201f547f8903033837bf4
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-09-04 22:18:52 +0530

    Create inline.py

---
+++ bot/plugins/inline.py
@@ -0,0 +1,58 @@
+"""
+vcVideoPlayer, Telegram Video Chat Bot
+Copyright (c) 2021  Zaute Km
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
+import asyncio
+from pyrogram.handlers import InlineQueryHandler
+from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client, errors
+from config import Config
+
+USERNAME = Config.BOT_USERNAME
+REPLY_MESSAGE = Config.REPLY_MESSAGE
+
+buttons = [
+            [
+                InlineKeyboardButton("🗣️ Feedback", url="https://t.me/zautebot"),
+                InlineKeyboardButton("Channel 📢", url="https://t.me/tgbotsproject"),
+            ],
+            [
+                InlineKeyboardButton("🤖 Deploy to Heroku 🤖", url="https://heroku.com/deploy?template=https://github.com/ZauteKm/vcVideoPlayer"),
+            ]
+         ]
+
+@Client.on_inline_query()
+async def search(client, query):
+    answers = []
+    if query.query == "SAF_ONE":
+        answers.append(
+            InlineQueryResultArticle(
+                title="Deploy Own Video Player Bot",
+                input_message_content=InputTextMessageContent(f"{REPLY_MESSAGE}\n\n<b>© Powered By : \n@tgbotsproject | @zautebot 👑</b>", disable_web_page_preview=True),
+                reply_markup=InlineKeyboardMarkup(buttons)
+                )
+            )
+        await query.answer(results=answers, cache_time=0)
+        return
+
+__handlers__ = [
+    [
+        InlineQueryHandler(
+            search
+        )
+    ]
+]
