54cf24552b582ba4349f5cca6c1d25064e0ef5c6
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-09-09 17:45:59 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -19,9 +19,19 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>
 import os
 import sys
 import asyncio
-from pyrogram import Client, idle
-from config import Config
+import subprocess
+from signal import SIGINT
+from asyncio import sleep
+from pyrogram import Client, filters, idle
+from threading import Thread
+from config import Config, db
+from pyrogram.types import Message
 from bot.plugins.nopm import User
+from bot.plugins.extras import USERNAME
+
+ADMINS = Config.ADMINS
+CHAT_ID = Config.CHAT_ID
+FFMPEG_PROCESSES = db.FFMPEG_PROCESSES
 
 Bot = Client(
     ":memory:",
@@ -33,11 +43,40 @@ Bot = Client(
 if not os.path.isdir("./downloads"):
     os.makedirs("./downloads")
 
+def stop_and_restart():
+    Bot.stop()
+    os.system("git pull")
+    sleep(10)
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+
+@Bot.on_message(filters.command(["restart", f"restart@{USERNAME}"]) & filters.user(ADMINS) & (filters.chat(CHAT_ID) | filters.private))
+def restart(client, m: Message):
+    k = m.reply_text("🔄 `Restarting ...`")
+    sleep(3)
+    process = FFMPEG_PROCESSES.get(CHAT_ID)
+    if process:
+        try:
+            process.send_signal(SIGINT)
+        except subprocess.TimeoutExpired:
+            process.kill()
+        except Exception as e:
+            print(e)
+            pass
+        FFMPEG_PROCESSES[CHAT_ID] = ""
+    Thread(
+        target=stop_and_restart
+        ).start()
+    try:
+        k.edit("✅ **Restarted Successfully! \nJoin @tgbotsproject For More!**")
+    except:
+        pass
+
 Bot.start()
 User.start()
-print("\nVideo Player Bot Started, Join @tgbotsproject")
+print("\nVideo Player Bot Started, Join @tgbotsproject!")
 
 idle()
 Bot.stop()
 User.stop()
-print("\nVideo Player Bot Stopped, Join @ZauteKm!")
+print("\nVideo Player Bot Stopped, Join @tgbotsproject!")
