95ffb470aa399d06a77374cc82b2903864494287
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-09-09 17:38:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,6 +22,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+class db:
+    VIDEO_CALL = {}
+    RADIO_CALL = {}
+    FFMPEG_PROCESSES = {}
+
 class Config:
     ADMIN = os.environ.get("AUTH_USERS", "")
     ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
@@ -30,10 +35,9 @@ class Config:
     CHAT_ID = int(os.environ.get("CHAT_ID", ""))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    SESSION_STRING = os.environ.get("SESSION_STRING", "")
     REPLY_MESSAGE = os.environ.get("REPLY_MESSAGE", "")
     if REPLY_MESSAGE:
         REPLY_MESSAGE = REPLY_MESSAGE
     else:
         REPLY_MESSAGE = None
-    SESSION_STRING = os.environ.get("SESSION_STRING", "")
