63fa7bcb668aa442b58c3d5c10fceb61bc6acbb7
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-09-10 16:32:49 +0530

    Update controls.py

--- plugins/controls.py
+++ plugins/controls.py
@@ -1,4 +1,19 @@
-from utils import get_playlist_str, get_admins, is_admin, restart, skip, pause, resume, volume, get_buttons, is_admin
+#!/usr/bin/env python3
+# Copyright (C) @zautekm
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from utils import get_playlist_str, get_admins, is_admin, restart_playout, skip, pause, resume, volume, get_buttons, is_admin
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from config import Config
@@ -89,4 +104,4 @@ async def replay_playout(client, m: Message):
     if not Config.CALL_STATUS:
         return await m.reply("Not Playing anything.")
     await m.reply_text(f"Replaying from begining")
-    await restart()
+    await restart_playout()
