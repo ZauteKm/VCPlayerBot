83b8912c8ab025f29f3de6dd05d200c9f03aaddf
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-09-10 16:21:02 +0530

    Update user.py

--- user.py
+++ user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (C) @subinps
+# Copyright (C) @zautekm
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
