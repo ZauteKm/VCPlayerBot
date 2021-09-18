6f709d94773dc358ba936e020dcee2bb1d98ec0e
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-09-18 08:01:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/ZauteKm/vcVideoPlayer /vcVideoPlayer
-cd /vcVideoPlayer
+git clone https://github.com/ZauteKm/VCVideoPlayBot /VCVideoPlayBot
+cd /VCVideoPlayBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
