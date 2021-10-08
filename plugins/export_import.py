814b92f838c21b3099cc3815914acc4578e8f567
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-10-08 17:55:53 +0530

    Update export_import.py

--- plugins/export_import.py
+++ plugins/export_import.py
@@ -50,7 +50,7 @@ async def export_play_list(client, message: Message):
     file=f"{message.chat.id}_{message.message_id}.json"
     with open(file, 'w+') as outfile:
         json.dump(Config.playlist, outfile, indent=4)
-    await client.send_document(chat_id=message.chat.id, document=file, file_name="PlayList.json", caption=f"Playlist\n\nNumber Of Songs: <code>{len(Config.playlist)}</code>\n\nJoin [XTZ Bots](https://t.me/subin_works)")
+    await client.send_document(chat_id=message.chat.id, document=file, file_name="PlayList.json", caption=f"Playlist\n\nNumber Of Songs: <code>{len(Config.playlist)}</code>\n\nJoin [TGBotsProJect](https://t.me/TGBotsProJect)")
     try:
         os.remove(file)
     except:
