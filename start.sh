203916ab6f61ada1a6f0b5f8e12db9b114384388
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-11-05 17:03:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
-  echo "Cloning main branch...."
-  git clone https://github.com/ZauteKm/VCVideoPlayBot /VCVideoPlayBot
+  echo "Cloning master branch...."
+  git clone https://github.com/ZauteKm/VCPlayerBot /VCPlayerBot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/ZauteKm/VCVideoPlayBot -b $BRANCH /VCVideoPlayBot
+  git clone https://github.com/ZauteKm/VCPlayerBot -b $BRANCH /VCPlayerBot
 fi
-cd /VCVideoPlayBot
+cd /VCPlayerBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
